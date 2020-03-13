41b16bc0424e5e2e1372cc8c8a23fa535b8f4406
Ho Tin Nghia Le <louis9286@icloud.com>
2020-03-12 19:09:01 -0500

    Initial Commit

---
+++ TAX_COLLECTION/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TAX_COLLECTION
+//
+//  Created by Long Nguyen on 3/12/20.
+//  Copyright © 2020 Ho Tin Nghia Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
